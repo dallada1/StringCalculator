f2456a3605b408659a5e9b5e20f9af469db7f65c
David Allada <dallada@qualtrax.com>
2017-05-17 10:10:39 -0400

    Adding Two Numbers

---
+++ StringCalculatorKata/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class Calculator
+    {
+        public Int32 Add(String input)
+        {
+            var firstNumber = 0;
+            var secondNumber = 0;
+
+            if (!String.IsNullOrWhiteSpace(input))
+            {
+                if(!input.Contains(","))
+                {
+                    firstNumber = Convert.ToInt32(input);
+                }
+                else
+                {
+                    var indexOfComma = input.IndexOf(',');
+                    var lengthOfSecondNumber = input.Length - indexOfComma - 1;
+                    firstNumber = Convert.ToInt32(input.Substring(0, indexOfComma));
+                    secondNumber = Convert.ToInt32(input.Substring(indexOfComma + 1, lengthOfSecondNumber));
+                }
+            }
+            
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ UnitStringCalculatorKataTests/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorKata;
+
+namespace UnitStringCalculatorKataTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        public CalculatorTests()
+        {
+            calculator = new Calculator();
+        }
+        [TestMethod]
+        public void EmptyStringReturnsZero()
+        {
+            Assert.AreEqual(0, calculator.Add(String.Empty));
+        }
+
+        [TestMethod]
+        public void SpacesReturnZero()
+        {
+            Assert.AreEqual(0, calculator.Add(" "));
+        }
+
+        [TestMethod]
+        public void OneReturnsOne()
+        {
+            Assert.AreEqual(1, calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void ElevenReturnsEleven()
+        {
+            Assert.AreEqual(11, calculator.Add("11"));
+        }
+
+        [TestMethod]
+        public void OneAndTwoReturnThree()
+        {
+            Assert.AreEqual(3, calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void ElevenAndElevenReturnsTwentyTwo()
+        {
+            Assert.AreEqual(22, calculator.Add("11,11"));
+        }
+    }
+}
